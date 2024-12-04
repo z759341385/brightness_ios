5bf33e7652478ea83f2b92841bd195551525cb3b
zhc <zhanghongchen1998@126.com>
2024-12-04 21:03:00 +0800

    Initial Commit

---
+++ brightness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  brightness
+//
+//  Created by zhc on 2024/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ brightness/brightnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  brightnessApp.swift
+//  brightness
+//
+//  Created by zhc on 2024/12/4.
+//
+
+import SwiftUI
+
+@main
+struct brightnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
