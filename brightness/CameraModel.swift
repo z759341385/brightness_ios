33eb98f8c10cee133e6ec3fffcee2891b92615f1
Zhanghongchen <zhanghongchen1998@126.com>
2024-12-05 11:36:44 +0800

    exif info

--- brightness/CameraModel.swift
+++ brightness/CameraModel.swift
@@ -1,6 +1,7 @@
 import AVFoundation
 import UIKit
 import Photos
+import ImageIO
 
 class CameraModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
     @Published var session = AVCaptureSession()
@@ -103,8 +104,74 @@ class CameraModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
             
             PHPhotoLibrary.shared().performChanges({
                 let request = PHAssetCreationRequest.forAsset()
-                if let imageData = image.jpegData(compressionQuality: 1.0) {
-                    request.addResource(with: .photo, data: imageData, options: nil)
+                
+                if let imageData = image.jpegData(compressionQuality: 1.0),
+                   let source = CGImageSourceCreateWithData(imageData as CFData, nil),
+                   let metadata = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [String: Any] {
+                    
+                    var finalMetadata = metadata
+                    var exifDict = (finalMetadata[kCGImagePropertyExifDictionary as String] as? [String: Any]) ?? [:]
+                    
+                    if let device = self.device {
+                        // 基本相机参数
+                        exifDict[kCGImagePropertyExifISOSpeedRatings as String] = [device.iso]
+                        exifDict[kCGImagePropertyExifExposureTime as String] = CMTimeGetSeconds(device.exposureDuration)
+                        exifDict[kCGImagePropertyExifFNumber as String] = device.lensAperture
+                        
+                        // 焦距相关
+                        let focalLength = device.activeFormat.videoZoomFactorUpscaleThreshold
+                        exifDict[kCGImagePropertyExifFocalLength as String] = focalLength
+                        exifDict["FocalLengthIn35mmFilm"] = Int(focalLength * 35.0) // 35mm等效焦距
+                        
+                        // 曝光相关
+                        exifDict[kCGImagePropertyExifBrightnessValue as String] = device.exposureTargetBias
+                        exifDict[kCGImagePropertyExifExposureBiasValue as String] = device.exposureTargetBias
+                        exifDict[kCGImagePropertyExifShutterSpeedValue as String] = log2(CMTimeGetSeconds(device.exposureDuration))
+                        exifDict[kCGImagePropertyExifApertureValue as String] = log2(pow(device.lensAperture, 2))
+                        
+                        // 曝光程序
+                        exifDict["ExposureProgram"] = 2 // 通常程序模式
+                        
+                        // 场景类型
+                        exifDict["SceneCaptureType"] = 0 // 标准
+                        
+                        // 曝光模式
+                        exifDict[kCGImagePropertyExifExposureMode as String] = device.exposureMode == .custom ? 1 : 0
+                        
+                        // 白平衡
+                        exifDict[kCGImagePropertyExifWhiteBalance as String] = device.whiteBalanceMode == .locked ? 1 : 0
+                        
+                        // 测光模式
+                        exifDict["MeteringMode"] = 5 // 评价测光
+                        
+                        // 时间信息
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "yyyy:MM:dd HH:mm:ss"
+                        let currentDate = dateFormatter.string(from: Date())
+                        exifDict[kCGImagePropertyExifDateTimeOriginal as String] = currentDate
+                        exifDict[kCGImagePropertyExifDateTimeDigitized as String] = currentDate
+                        
+                        // 设备信息
+                        var tiffDict = (finalMetadata[kCGImagePropertyTIFFDictionary as String] as? [String: Any]) ?? [:]
+                        tiffDict[kCGImagePropertyTIFFMake as String] = "Apple"
+                        tiffDict[kCGImagePropertyTIFFModel as String] = UIDevice.current.model
+                        tiffDict[kCGImagePropertyTIFFSoftware as String] = "\(UIDevice.current.systemName) \(UIDevice.current.systemVersion)"
+                        tiffDict[kCGImagePropertyTIFFDateTime as String] = currentDate
+                        
+                        finalMetadata[kCGImagePropertyTIFFDictionary as String] = tiffDict
+                        finalMetadata[kCGImagePropertyExifDictionary as String] = exifDict
+                    }
+                    
+                    let mutableData = NSMutableData()
+                    if let destination = CGImageDestinationCreateWithData(mutableData, 
+                                                                        UTType.jpeg.identifier as CFString, 
+                                                                        1, 
+                                                                        nil) {
+                        CGImageDestinationAddImageFromSource(destination, source, 0, finalMetadata as CFDictionary)
+                        CGImageDestinationFinalize(destination)
+                        
+                        request.addResource(with: .photo, data: mutableData as Data, options: nil)
+                    }
                 }
             }) { success, error in
                 DispatchQueue.main.async {
